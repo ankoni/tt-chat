09e6de1aa43310a1e8ef4e6e0b508a35ce7e479e
ankoni <bakisovaa@gmail.com>
2024-09-07 18:49:04 +0700

    Верстка чата

--- src/app/components/main-header/main-header.component.ts
+++ src/app/components/main-header/main-header.component.ts
@@ -1,13 +1,15 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core'
 import { MatIconButton } from '@angular/material/button'
 import { MatIcon } from '@angular/material/icon'
+import { MatTooltip } from '@angular/material/tooltip'
 
 @Component({
     selector: 'app-main-header',
     standalone: true,
     imports: [
         MatIconButton,
-        MatIcon
+        MatIcon,
+        MatTooltip
     ],
     templateUrl: './main-header.component.html',
     styleUrl: './main-header.component.scss',
